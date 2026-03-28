7605f00881da300108e8f9431f31a74e3067b362
nailed1 <max20070608@gmail.com>
2026-03-28 17:30:47 +0800

    changed solvation

--- trajectory/main.py
+++ trajectory/main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import skimage.measure as measure
 
-# Ваши функции из первого кода
+
 def area(labeled, label):
     return (labeled == label).sum()
 
@@ -22,16 +22,16 @@ def distance_to_last(last, target_point):
     distance = sum_of_squares ** 0.5
     return distance
 
-# Словарь для хранения траекторий (как во втором коде)
+
 trajectories = {}
 
-# Цикл по всем 100 кадрам
+
 for i in range(100):
     image = np.load(f"trajectory/out/h_{i}.npy")
     labeled = measure.label(image, background=0)
     max_label = labeled.max()
     
-    # Получаем центроиды всех объектов в текущем кадре
+    
     centroids = []
     for j in range(1, max_label + 1):
         center = centroid(labeled, j)
@@ -39,18 +39,18 @@ for i in range(100):
             centroids.append(center)
     
     if i == 0:
-        # Инициализация траекторий в первом кадре
+        
         for j, center in enumerate(centroids):
             trajectories[j] = [center]
     else:
-        # Сопоставление объектов с существующими траекториями
+        
         for cur_cord in centroids:
             min_num = 0
             min_dist = float('inf')
             
             for num, cord in trajectories.items():
                 last_cord = cord[-1]
-                # Используем вашу функцию расстояния
+                
                 dist = distance_to_last(last_cord, cur_cord)
                 
                 if min_dist > dist:
@@ -59,7 +59,7 @@ for i in range(100):
             
             trajectories[min_num].append(cur_cord)
 
-# Отрисовка всех траекторий
+
 for num, obj in trajectories.items():
     cord = np.array(obj)
     plt.plot(cord[:, 1], cord[:, 0], marker='o', label=f'Track {num}')
