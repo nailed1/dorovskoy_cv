8684ee9f730f4b39aa1117b32fd8cde876ecea24
nailed1 <max20070608@gmail.com>
2026-03-28 22:43:26 +0800

    resolved star task

--- stars/main.py
+++ stars/main.py
@@ -1,16 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from skimage.measure import label
 
 image = np.load("/Users/nailed1/Documents/GitHub/dorovskoy_cv/stars/stars.npy")
 
 def area(labeled, label):
     return (labeled == label).sum()
 
-def neighbours4(y ,x):
-    return (y, x+1), (y+1, x), (y-1, x), (y, x-1)
+def neighbours4(y, x):
+    return [(y, x+1), (y+1, x), (y-1, x), (y, x-1)]
 
 def neighboursX(y, x):
-    return (y-1, x+1), (y+1, x+1), (y+1, x-1), (y-1, x-1)
+    return [(y-1, x+1), (y+1, x+1), (y+1, x-1), (y-1, x-1)]
 
 def neighbours8(y, x):
     return neighbours4(y, x) + neighboursX(y, x)
@@ -25,10 +26,7 @@ def is_star(neighboursX):
 def is_cross(neighbours4):
     return all(n == 1 for n in neighbours4)
 
-# Label connected components
-from scipy.ndimage import label
-
-labeled, num_features = label(image)
+labeled, num_features = label(image, return_num=True)
 
 star_count = 0
 cross_count = 0
@@ -37,18 +35,26 @@ for label_id in range(1, num_features + 1):
     ys, xs = np.where(labeled == label_id)
     cy, cx = int(np.round(np.mean(ys))), int(np.round(np.mean(xs)))
     
-    # Get neighbors at centroid
-    n4_coords = neighbours4(cy, cx)
-    nX_coords = neighboursX(cy, cx)
+    height, width = labeled Check 4-connectivity neighbors (for crosses)
+    n4_values = []
+    for dy, dx in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+        ny, nx = cy + dy, cx + dx
+        if 0 <= ny < height and 0 <= nx < width:
+            n4_values.append(labeled[ny, nx])
+        else:
+            n4_values.append(0)
     
-    neighbours4_vals = [labeled[y, x] == label_id if 0 <= y < labeled.shape[0] and 0 <= x < labeled.shape[1] else False 
-                        for y, x in n4_coords]
-    neighboursX_vals = [labeled[y, x] == label_id if 0 <= y < labeled.shape[0] and 0 <= x < labeled.shape[1] else False 
-                        for y, x in nX_coords]
+    nX_values = []
+    for dy, dx in [(-1, 1), (1, 1), (1, -1), (-1, -1)]:
+        ny, nx = cy + dy, cx + dx
+        if 0 <= ny < height and 0 <= nx < width:
+            nX_values.append(labeled[ny, nx])
+        else:
+            nX_values.append(0)
     
-    if is_star(neighboursX_vals):
+    if all(val == label_id for val in nX_values):
         star_count += 1
-    elif is_cross(neighbours4_vals):
+    elif all(val == label_id for val in n4_values):
         cross_count += 1
 
 print(f"Stars: {star_count}, Crosses: {cross_count}")
