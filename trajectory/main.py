7c7e8a350ceb44033f36047993f5205b996ccd9c
nailed1 <max20070608@gmail.com>
2026-03-28 17:00:09 +0800

    resolved a task

--- trajectory/main.py
+++ trajectory/main.py
@@ -2,20 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import skimage.measure as measure
 
-image = np.load("/Users/nailed1/Documents/GitHub/dorovskoy_cv/trajectory/out/h_96.npy")
-
-labeled = measure.label(image, background=0)
-max_label = labeled.max()
-
+# Ваши функции из первого кода
 def area(labeled, label):
     return (labeled == label).sum()
-
-areas = []
-for i in range(1, max_label + 1):
-    S = area(labeled, i)
-    areas.append(S)
-
-print(len(areas))
 
 def centroid(labeled, label):
     coords = np.argwhere(labeled == label)
@@ -24,52 +13,56 @@ def centroid(labeled, label):
     else:
         return None
 
-centroids = []
-for i in range(1, max_label + 1):
-    center = centroid(labeled, i)
-    if center is not None:
-        centroids.append(center)
-
-print(centroids)
-def distance_to_last(i):
-    target_point = centroids[i]
-
+def distance_to_last(last, target_point):
     dy = last[0] - target_point[0]
     dx = last[1] - target_point[1]
     dx_squared = dx ** 2
     dy_squared = dy ** 2
-
     sum_of_squares = dy_squared + dx_squared
-
     distance = sum_of_squares ** 0.5
-
     return distance
 
-visited = [0]
-while len(visited) < len(centroids):
-    last = centroids[visited[-1]]
+# Словарь для хранения траекторий (как во втором коде)
+trajectories = {}
 
-    possible_indices = []
-    for i in range(len(centroids)):
-        if i not in visited:
-            possible_indices.append(i)
+# Цикл по всем 100 кадрам
+for i in range(100):
+    image = np.load(f"trajectory/out/h_{i}.npy")
+    labeled = measure.label(image, background=0)
+    max_label = labeled.max()
+    
+    # Получаем центроиды всех объектов в текущем кадре
+    centroids = []
+    for j in range(1, max_label + 1):
+        center = centroid(labeled, j)
+        if center is not None:
+            centroids.append(center)
+    
+    if i == 0:
+        # Инициализация траекторий в первом кадре
+        for j, center in enumerate(centroids):
+            trajectories[j] = [center]
+    else:
+        # Сопоставление объектов с существующими траекториями
+        for cur_cord in centroids:
+            min_num = 0
+            min_dist = float('inf')
+            
+            for num, cord in trajectories.items():
+                last_cord = cord[-1]
+                # Используем вашу функцию расстояния
+                dist = distance_to_last(last_cord, cur_cord)
+                
+                if min_dist > dist:
+                    min_dist = dist
+                    min_num = num
+            
+            trajectories[min_num].append(cur_cord)
 
-    best_index_so_far = possible_indices[0]
-    smallest_distance_so_far = distance_to_last(best_index_so_far)
+# Отрисовка всех траекторий
+for num, obj in trajectories.items():
+    cord = np.array(obj)
+    plt.plot(cord[:, 1], cord[:, 0], marker='o', label=f'Track {num}')
 
-    for i in possible_indices[1:]:
-        current_distance = distance_to_last(i)
-        if current_distance < smallest_distance_so_far:
-            smallest_distance_so_far = current_distance
-            best_index_so_far = i
-
-    next_i = best_index_so_far
-
-    visited.append(next_i)
-
-
-trajectory = np.array([centroids[i] for i in visited])
-
-plt.imshow(image)
-plt.plot(trajectory[:, 1], trajectory[:, 0], 'r-o', linewidth=2, markersize=4)
+plt.legend()
 plt.show()
